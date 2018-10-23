b181f9692cbb520455a621bb157870c0eb4e70a3
Jeremy <je.barrette@hotmail.com>
2018-10-23 15:11:11 -0400

    TP1 - Actual work

--- TP1/Server/src/Server.java
+++ TP1/Server/src/Server.java
@@ -7,118 +7,180 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Paths;
+import java.nio.file.Path;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-/**
- * A server program which accepts requests from clients to
- * capitalize strings.  When clients connect, a new thread is
- * started to handle an interactive dialog in which the client
- * sends in a string and the server thread sends back the
- * capitalized version of the string.
- *
- * The program is runs in an infinite loop, so shutdown in platform
- * dependent.  If you ran it from a console window with the "java"
- * interpreter, Ctrl+C generally will shut it down.
- */
 public class Server {
 
-    /**
-     * Application method to run the server runs in an infinite loop
-     * listening on port 5000.  When a connection is requested, it
-     * spawns a new thread to do the servicing and immediately returns
-     * to listening.  The server keeps a unique client number for each
-     * client that connects just to show interesting logging
-     * messages.  It is certainly not necessary to do this.
-     */
     public static void main(String[] args) throws Exception {
-
+        JFrame frame = new JFrame("TP1: Storage Drive");
+        Boolean CorrectAddress = false;
+        String serverAddress = "";
+        String[] parsedAddress;
+        String message = "";
         int clientNumber = 0;
 
-        JFrame frame = new JFrame("Capitalize Server");
+        while (!CorrectAddress) {
+            serverAddress = JOptionPane.showInputDialog(frame, "Enter IP Address and Port of the Server:\n Format (Address:Port)\n" + message, "Welcome to the Storage Drive", JOptionPane.QUESTION_MESSAGE);
+            message = testAddress(serverAddress);
+            if (message == "") {
+                CorrectAddress = true;
+            }
+        }
 
-        // Get the server address from a dialog box.
-        String serverAddress = JOptionPane.showInputDialog(frame,"Enter IP Address of the Server:","Welcome to the Capitalization Program", JOptionPane.QUESTION_MESSAGE);
-        int port = 5000;
+        parsedAddress = serverAddress.split(":");
 
-		ServerSocket listener;
-		InetAddress locIP = InetAddress.getByName(serverAddress);
-		listener = new ServerSocket();
-		listener.setReuseAddress(true);
-		listener.bind(new InetSocketAddress(locIP, port));
+        ServerSocket listener;
+        InetAddress locIP = InetAddress.getByName(parsedAddress[0]);
+        int port = Integer.parseInt(parsedAddress[1]);
 
-        System.out.format("The capitalization server is running on %s:%d%n", serverAddress, port);
+        listener = new ServerSocket();
+        listener.setReuseAddress(true);
+        listener.bind(new InetSocketAddress(locIP, port));
+
+        System.out.println("Storage Drive Server connected on " + serverAddress);
 
         try {
             while (true) {
-                new Capitalizer(listener.accept(), clientNumber++).start();
+                new StorageDrive(listener.accept(), clientNumber++).start();
             }
         } finally {
             listener.close();
         }
+
     }
 
-    /**
-     * A private thread to handle capitalization requests on a particular
-     * socket.  The client terminates the dialogue by sending a single line
-     * containing only a period.
-     */
-    private static class Capitalizer extends Thread {
+    private static class StorageDrive extends Thread {
         private Socket socket;
         private int clientNumber;
+        private Path path;
 
-        public Capitalizer(Socket socket, int clientNumber) {
+        public StorageDrive(Socket socket, int clientNumber) {
             this.socket = socket;
             this.clientNumber = clientNumber;
-            log("New connection with client# " + clientNumber + " at " + socket);
+            this.path = Paths.get("");
+            this.path = this.path.toAbsolutePath();
+            System.out.println("New connection with client# " + clientNumber + " at " + socket);
         }
 
-        /**
-         * Services this thread's client by first sending the
-         * client a welcome message then repeatedly reading strings
-         * and sending back the capitalized version of the string.
-         */
         public void run() {
             try {
-
-                // Decorate the streams so we can send characters
-                // and not just bytes.  Ensure output is flushed
-                // after every newline.
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
-                // Send a welcome message to the client.
-                out.println("Hello, you are client #" + clientNumber + ".");
-                out.println("Enter a line with only a period to quit\n");
+                System.out.println("Welcome to the Storage Drive. Enter \"Exit\" to leave\n");
+                System.out.println("Path: " + this.path);
+                String returnMessage = "";
 
-                // Get messages from the client, line by line; return them
-                // capitalized
-                while (true) {
+                while(true) {
                     String input = in.readLine();
-                    if (input == null || input.equals(".")) {
+                    if (input == "Exit") {
                         break;
                     }
-                    out.println(input.toUpperCase());
+                    returnMessage = testCommand(input);
+                    out.println(returnMessage);
                 }
+
             } catch (IOException e) {
-                log("Error handling client# " + clientNumber + ": " + e);
+                System.out.println("Error handling client# " + clientNumber + ": " + e);
             } finally {
                 try {
                     socket.close();
                 } catch (IOException e) {
-                    log("Couldn't close a socket, what's going on?");
+                    System.out.println("Couldn't close a socket, what's going on?");
                 }
-                log("Connection with client# " + clientNumber + " closed");
+                System.out.println("Connection with client# " + clientNumber + " closed");
             }
         }
+    }
 
-        /**
-         * Logs a simple message.  In this case we just write the
-         * message to the server applications standard output.
-         */
-        private void log(String message) {
-            System.out.println(message);
+    private static String testCommand(String command) {
+        String[] parsedCommand = command.split(" ");
+
+        if (parsedCommand.length > 2) {
+            return "Command format with too many arguments";
+        } else if (parsedCommand.length < 2) {
+            return "Command format with too few arguments";
+        }
+
+        if (parsedCommand[0] == "cd") {
+            return command_cd(parsedCommand[1]);
+        } else if (parsedCommand[0] == "ls") {
+            return command_ls(parsedCommand[1]);
+        } else if (parsedCommand[0] == "mkdir") {
+            return commmand_mkdir(parsedCommand[1]);
+        } else if (parsedCommand[0] == "upload") {
+            return command_upload(parsedCommand[1]);
+        } else if (parsedCommand[0] == "download") {
+            return command_download(parsedCommand[1]);
+        } else {
+            return "Command not recognized";
         }
     }
+
+    private static String command_cd(String command) {
+        return "cd";
+    }
+
+    private static String command_ls(String command) {
+        return "ls";
+    }
+
+    private static String commmand_mkdir(String command) {
+        return "mkdir";
+    }
+
+    private static String command_upload(String command) {
+        return "upload";
+    }
+
+    private static String command_download(String command) {
+        return "download";
+    }
+
+    private static String testAddress(String serverAddress) {
+        String[] addressPort;
+        String[] parsedAddress;
+        int temp = 0;
+
+        addressPort = serverAddress.split(":");
+        parsedAddress = addressPort[0].split("\\.");
+
+        if(addressPort.length != 2) {
+            return "Error: No identified port (:)";
+        } else if(parsedAddress.length != 4) {
+            return "Error: Wrong format address";
+        } else {
+            try{
+                for (int i = 0; i < 4; i++) {
+                    temp = Integer.parseInt(parsedAddress[i]);
+                    if (temp < 0 || temp > 255) {
+                        return "Error: Byte #" + (i + 1) + "Ouside limits";
+                    }
+                }
+
+                temp = Integer.parseInt(addressPort[1]);
+                if (temp < 5000 || temp > 5050) {
+                    return "Error: Port outside limit";
+                }
+
+
+            } catch(Exception e) {
+                return "Invalid character in the address";
+            }
+
+            return "";
+        }
+
+    }
+
+    private static class Capitalizer extends Thread {
+
+    }
+
+
+
 }
